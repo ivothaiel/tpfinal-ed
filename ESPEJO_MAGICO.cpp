bfcfd7c9c856057ab0e39fb7cfed14634237082b
DominguezEmanuel <dmanu401@gmail.com>
2025-11-14 19:53:38 -0300

    Agrego avance de los menus del juego

---
+++ ESPEJO_MAGICO.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+using namespace std;
+
+// Diferentes menus del juego
+#include "menus.hpp"
+
+int main(){
+	char op;
+	do{
+		system("cls");
+		menu_principal(op);
+		switch(op){
+		case '1':
+			break;
+		case '2':
+			break;
+		case '3':
+			break;
+		case '4':
+			break;
+		case '5':
+			cout << "\n";
+			cout << "Que los espiritus guardianes guien tu camino.\n";
+			cout << "Hasta pronto, viajero.\n\n";
+			break;
+		default:
+			cout << "OPCION INVALIDA" << endl;
+		}
+		system("pause");
+	} while(op != '5');
+	return 0;
+}
---
+++ menus.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+void menu_principal(char &op){
+	cout << "\n\n";
+	cout << "+-------------------------------------------------------+\n";
+	cout << "|                                                       |\n";
+	cout << "|          EL HECHIZO DEL ESPEJO                        |\n";
+	cout << "|                                                       |\n";
+	cout << "+-------------------------------------------------------+\n";
+	cout << "\n";
+	
+	cout << "  -----------------------------------------------\n";
+	cout << "   Hace mas de 500 años, una princesa fue\n";
+	cout << "   atrapada en un espejo maldito. Solo quien\n";
+	cout << "   descubra las 6 palabras magicas podra romper\n";
+	cout << "   el hechizo y liberarla...\n";
+	cout << "  -----------------------------------------------\n\n";
+	
+	cout << "* * * Elige una opcion para comenzar * * *\n\n";
+	
+	cout << "1)  Gestionar Jugadores\n";
+	cout << "2)  Gestionar Palabras\n";
+	cout << "3)  Iniciar Juego\n";
+	cout << "4)  Vencedores\n";
+	cout << "5)  Salir\n\n";
+	
+	cout << "> Ingresa tu opcion: ";
+	cin >> op;
+}
